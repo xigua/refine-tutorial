9af485a5a20139f14d17f4ddd9dee97e18db0c58
xigua <xigua@me.com>
2024-01-12 00:04:08 +0800

    use AntdInferencer to auto-create pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
 import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
-
-import { useNotificationProvider } from "@refinedev/antd";
+import { AntdInferencer } from '@refinedev/inferencer/antd';
+import { ThemedLayoutV2, useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
 import routerBindings, {
   DocumentTitleHandler,
+  NavigateToResource,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
 import dataProvider from "@refinedev/simple-rest";
 import { App as AntdApp } from "antd";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import { ColorModeContextProvider } from "./contexts/color-mode";
 
 function App() {
   return (
     <BrowserRouter>
-      <GitHubBanner />
+      {/* <GitHubBanner /> */}
       <RefineKbarProvider>
         <ColorModeContextProvider>
           <AntdApp>
@@ -32,9 +33,27 @@ function App() {
                   useNewQueryKeys: true,
                   projectId: "s4F0XQ-7aRQcl-gBOp3s",
                 }}
+                resources={[
+                  {
+                    name: "blog_posts",
+                    list: "/blog-posts",
+                    show: "/blog-posts/show/:id",
+                    create: "/blog-posts/create",
+                    edit: "/blog-posts/edit/:id",
+                  }
+                ]}
               >
                 <Routes>
-                  <Route index element={<WelcomePage />} />
+                  <Route element={<ThemedLayoutV2><Outlet /></ThemedLayoutV2>}>
+                    {/* <Route index element={<WelcomePage />} /> */}
+                    <Route index element={<NavigateToResource resource='blog_posts' />} />
+                    <Route path='blog-posts'>
+                      <Route index element={<AntdInferencer />} />
+                      <Route path='show/:id' element={<AntdInferencer />} />
+                      <Route path='edit/:id' element={<AntdInferencer />} />
+                      <Route path='create' element={<AntdInferencer />} />
+                    </Route>
+                  </Route>
                 </Routes>
                 <RefineKbar />
                 <UnsavedChangesNotifier />
